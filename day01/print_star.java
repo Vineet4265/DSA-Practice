f23f05e5c884844b73a083e486c9a76129ff1d9a
Vineet4265 <vineetwork89@gmail.com>
2026-04-04 13:34:47 +0530

    day01: star

---
+++ day01/print_star.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class print_star {
+    public static void main(String[] args) {
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+    
+}
