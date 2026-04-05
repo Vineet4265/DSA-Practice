e188a74eb0c2bd3974382a60729711731fa67c87
Vineet4265 <vineetwork89@gmail.com>
2026-04-05 09:37:47 +0530

    addition

---
+++ day02/sum_program.java
@@ -0,0 +1,10 @@
+package day02;
+
+public class sum_program {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
