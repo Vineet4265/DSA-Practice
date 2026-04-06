09b6b4e715387e50fe6b037aa7f5f84de9646a6d
Vineet4265 <vineetwork89@gmail.com>
2026-04-06 10:16:43 +0530

    ternary_operator

---
+++ day03/ternary_operator.java
@@ -0,0 +1,9 @@
+package day03;
+
+public class ternary_operator {
+    public static void main(String args[]){
+        int number = 5;
+        String type = ((number % 2) == 0)?"even":"odd";
+        System.out.println(type);
+    }
+}
