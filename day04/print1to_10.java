fda1fdff28f724c1a6fc813db81bf607b030d217
Vineet4265 <vineetwork89@gmail.com>
2026-04-07 11:10:29 +0530

    print1to_10.java

---
+++ day04/print1to_10.java
@@ -0,0 +1,11 @@
+package day04;
+
+public class print1to_10 {
+    public static void main(String args[]){
+        int counter = 1;
+        while(counter<=10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
