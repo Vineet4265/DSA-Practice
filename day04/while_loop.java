d45fe43e312d2aa0110c4d3e771f8db473c739a6
Vineet4265 <vineetwork89@gmail.com>
2026-04-07 10:59:13 +0530

    while_loop

---
+++ day04/while_loop.java
@@ -0,0 +1,12 @@
+package day04;
+
+public class while_loop {
+    public static void main(String args[]){
+        int counter = 0;
+        while(counter<5){
+            System.out.println("Hello World");
+            counter++;
+        }
+        System.out.println("printed hello word five times");
+    }
+}
