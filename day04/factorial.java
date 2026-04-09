514ef2f22a40a87ae913b2b3f73b42a9af806a24
Vineet4265 <vineetwork89@gmail.com>
2026-04-09 10:06:30 +0530

    factorial

---
+++ day04/factorial.java
@@ -0,0 +1,15 @@
+package day04;
+import java.util.*;
+public class factorial {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int fact = 1;
+        System.out.println("Enter any positive integer");
+        num = sc.nextInt();
+        for(int i =1; i<=num; i++){
+            fact *= i;
+        }
+        System.out.println("Factorial of a number is "+fact);
+    }
+}
