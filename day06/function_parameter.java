ea833ca9ad4349a95c7ac63df635ca8620832435
Vineet4265 <vineetwork89@gmail.com>
2026-04-10 10:19:13 +0530

    function_parameter

---
+++ day06/function_parameter.java
@@ -0,0 +1,15 @@
+package day06;
+import java.util.*;
+public class function_parameter {
+    public static void calculateSum(int a, int b){
+        int sum = a+b;
+        System.out.println("Sum is " +sum);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        calculateSum(a, b);
+        sc.close();
+    }
+}
